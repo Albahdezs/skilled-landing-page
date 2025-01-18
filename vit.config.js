7c993835a9ff67f21b139387d60fac8dc5ed5a02
Albahdezs <alba.hdez.serr@gmail.com>
2025-01-18 15:23:22 +0100

    Comit inicial

---
+++ vit.config.js
@@ -0,0 +1,6 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/skilled-landing-page/',  // Reemplaza 'nombre-del-repo' con el nombre de tu repositorio
+})
